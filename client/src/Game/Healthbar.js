1650b69ea0266f15644534f3e6afbb7e7f96ecc4
admin <m.siegrist92@gmail.com>
2023-12-27 09:50:36 -0700

    feature : Healthbar reworked to use background-image css and position-x property to show health bar pct in background, text is placed within heathbar, saving space and improving readability

--- client/src/Game/Healthbar.js
+++ client/src/Game/Healthbar.js
@@ -1,23 +1,35 @@
 const Healthbar = ({max_hp, current_hp, block}) => {
-  const style = {
-    backgroundColor: "red",
-    width: ((current_hp / max_hp) * 100).toFixed(2) + "%"
+  const full_hp_pos = 100
+  const zero_hp_pos = 280
+  const increment = (zero_hp_pos - full_hp_pos) / 100
+
+  const healthbar_position = zero_hp_pos - (((current_hp / max_hp) * 100) * increment)
+
+  const health_bar_style = {
+    backgroundImage: "url('just_red.png')",
+    backgroundColor: "grey",
+    backgroundRepeat: "no-repeat",
+    backgroundPositionX: `${healthbar_position}%`,
+    height: "20px",
+    width: "250px"
+  }
+
+  const block_style = {
+    backgroundImage: "url('block_icon.jpg')",
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+    color: "cyan",
+    backgroundPositionX: "center",
+
   }
 
   return (
-    <div style={{display: "inline-block", width: "90%"}}>
-      <div style={style}><p style={{color: "red", marginTop: "2px"}}>&nbsp;</p></div>
-      <div className="grid two_col_60_40" style={{height: "60px"}}>
-        <p className="center_text"><b>{current_hp}</b> / {max_hp}</p>
-        <div style={{display: "flex"}}>
-          {block > 0 &&
-            <>
-            <img src="block_icon.jpg" style={{display: "block", margin: "0 auto", width: "45px"}}/>
-            <p className="center_text">{block}</p>
-            </>
-          }
+    <div className="grid m-2" style={{gridTemplateColumns: "85% 1fr", height: "35px", width: "300px"}}>
+      <div className="center_text p-2" style={health_bar_style}><b>{current_hp} / {max_hp}</b></div>
+      {block > 0 &&
+        <div className="center_text" style={block_style}><p className="m-4"><b>{block}</b></p>
         </div>
-      </div>
+      }
     </div>
   )
 }
